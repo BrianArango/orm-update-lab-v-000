c5c98cb95d55da6733ad8af699d5d44cada3bdba
Brian Arango <brianvictus@gmail.com>
2019-04-22 20:35:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,8 +37,7 @@ class Student
      end
    end
 
- 
-  end		   def self.create(name:, grade:)
+ 	def self.create(name:, grade:)
      student = Student.new(name, grade)
      student.save
      student
